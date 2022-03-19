22b178e584623a628dc5732161ba2153c025d099
Sayed Miah <m.s.miah@keele.ac.uk>
2022-03-19 14:52:29 +0000

    plotting normA conds more efficiently and tidier code

--- plot_normA_conds.py
+++ plot_normA_conds.py
@@ -1,11 +1,13 @@
 import numpy as np
 from NormA import NormA
 import os
+import plt_options
+import matplotlib.pyplot as plt
 
 
 def main():
     # ------------------------------------------------------------------
-    N_s = 180
+    N_s = 45
     layers = 2
     neurons = 16
     N_o = 40
@@ -22,17 +24,26 @@ def main():
     x_label = obj.x_label
     y_label = obj.y_label
 
-    if not os.path.exists(f'figures/{save_folder}'):
-        os.makedirs(f'figures/{save_folder}')
+    if not os.path.exists(f'{save_folder}'):
+        os.makedirs(f'{save_folder}')
 
     cond_factor_out = np.loadtxt(f'data/normA/{folder}/CondFactorOut.txt', skiprows=1, delimiter=",")
-    print(f"{cond_factor_out.shape}")
+    print(f"{cond_factor_out.shape=}")
     cond_factor_out_4k = cond_factor_out[:, 0]
 
     y_conds = list(np.split(y[0], len(cond_factor_out_4k), axis=0))
     labels = [f"NN conductivity = {i}" for i in cond_factor_out_4k]
 
-    plt_options
+    plt_options.main()
+    for i in range(len(y_conds)):
+        plt.plot(freq_out, y_conds[i], label=labels[i])
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.yscale("log")
+    plt.savefig(save_name)
+    print(f'Saved figure to {save_name}')
+    plt.show()
 
 
 if __name__ == "__main__":
--- plt_options.py
+++ plt_options.py
@@ -1,21 +1,23 @@
 import matplotlib.pyplot as plt
 
-# plot line colours, 10 colors all together
-CB91_Blue = '#2CBDFE'
-CB91_Green = '#47DBCD'
-CB91_Pink = '#F3A0F2'
-CB91_Purple = '#9D2EC5'
-CB91_Violet = '#661D98'
-CB91_Amber = '#F5B14C'
-CB91_Red = '#EA1200'
-CB91_Navy = '#1F618D'
-CB91_Grey = '#797D7f'
-CB91_Black = '#17202A'
 
-color_list = [CB91_Blue, CB91_Pink, CB91_Green, CB91_Amber,
-              CB91_Purple, CB91_Violet, CB91_Red, CB91_Navy, CB91_Grey, CB91_Black]
+def main():
+    # plot line colours, 10 colors all together
+    CB91_Blue = '#2CBDFE'
+    CB91_Green = '#47DBCD'
+    CB91_Pink = '#F3A0F2'
+    CB91_Purple = '#9D2EC5'
+    CB91_Violet = '#661D98'
+    CB91_Amber = '#F5B14C'
+    CB91_Red = '#EA1200'
+    CB91_Navy = '#1F618D'
+    CB91_Grey = '#797D7f'
+    CB91_Black = '#17202A'
 
-plt.rcParams['axes.prop_cycle'] = plt.cycler(color=color_list)
-plt.rcParams['lines.linewidth'] = 2
-plt.rcParams['axes.labelsize'] = 16
-plt.rcParams['axes.titlesize'] = 16
+    color_list = [CB91_Blue, CB91_Pink, CB91_Green, CB91_Amber,
+                  CB91_Purple, CB91_Violet, CB91_Red, CB91_Navy, CB91_Grey, CB91_Black]
+
+    plt.rcParams['axes.prop_cycle'] = plt.cycler(color=color_list)
+    plt.rcParams['lines.linewidth'] = 2
+    plt.rcParams['axes.labelsize'] = 16
+    plt.rcParams['axes.titlesize'] = 16
